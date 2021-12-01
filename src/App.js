b1d0b9d1ff6e2d89acb4f7f58613a5cd697114ab
conconartist <conconart.info@gmail.com>
2021-12-01 11:44:30 -0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/Header';
+
 const App = () => {
   return (
-    <h1>FitLit 2.0</h1>
+    <>
+    <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+      <header>
+          <h1>F I T L I T</h1>
+      </header>
+  )
+}
+
+export default Header;
