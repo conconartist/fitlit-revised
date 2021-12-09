44e3066fd9d267be736a74a8b32d209ad112e3ef
conconartist <conconart.info@gmail.com>
2021-12-09 10:40:25 -0700

    Add User

--- src/components/User.js
+++ src/components/User.js
@@ -2,10 +2,34 @@ import React, { useEffect } from 'react';
 
 const User = ({user}) => {
   return (
-      <section>
-          <h1>F I T L I T</h1>
-          <h2>Hi, {user}</h2>
-      </section>
+    <section class="user-main">
+    <div class="wrapper-top">
+      <article class="user-profile">
+        <h2>Your Profile</h2>
+          <p class="user-name"></p>
+          <p class="user-address"></p>
+          <p class="user-email"></p>
+          <p class="user-stride-length"></p>
+          <p class="user-friends-length"></p>
+      </article>
+      <div class="user-container">
+        <article class="user-latest-sleep-stats">
+          <h3>Last Night's Sleep Stats</h3>
+          <p class="sleep-quantity-last-night"></p>
+          <p class="sleep-quality-last-night"></p>
+        </article>
+        <article class="user-step-goal">
+          <h3>Your Daily Step Goal</h3>
+            <p class="user-step-goal-input"></p>
+        </article>
+        <article class="community-step-goal">
+          <h3>Community Average Step Goal</h3>
+          <p class="community-step-goal-input"></p>
+        </article>
+      </div>
+    </div>
+
+  </section>
   )
 }
 
